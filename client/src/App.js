0f8fee754fd96cd68ffab3b3c0e4acb412f7a9de
Sarang Padalkar <padalkar.sarang@gmail.com>
2021-02-28 14:44:10 -0500

    checkpoint restored

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3 with dcoker
+            Learn React 3
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
