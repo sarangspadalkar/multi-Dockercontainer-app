02b64ebad76eeea6be045e46abb3da90b9341235
Sarang Padalkar <padalkar.sarang@gmail.com>
2021-03-04 14:44:36 -0500

    added GKE configrations

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t sarangpadalkar/multi-client:latest -t sarangpadalkar/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t sarangpadalkar/multi-server:latest -t sarangpadalkar/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t sarangpadalkar/multi-worker:latest -t sarangpadalkar/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+docker push sarangpadalkar/multi-client:latest
+docker push sarangpadalkar/multi-server:latest
+docker push sarangpadalkar/multi-worker:latest
+
+docker push sarangpadalkar/multi-client:$SHA
+docker push sarangpadalkar/multi-server:$SHA
+docker push sarangpadalkar/multi-worker:$SHA
+
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=sarangpadalkar/multi-server:$SHA
+kubectl set image deployments/client-deployment client=sarangpadalkar/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=sarangpadalkar/multi-worker:$SHA
